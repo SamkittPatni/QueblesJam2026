80304e04d0ad9f9efe5a89d517989f22505af9ac
Samkitt Patni <samkitt.patni@gmail.com>
2026-02-05 16:35:25 +0000

    Add Player NPC interaction

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -2,20 +2,45 @@ using UnityEngine;
 
 public class PlayerInteract : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private int currentNPC = -1; // -1 means no NPC in range
+
+    private void OnInteract()
     {
-        
+        if (currentNPC != -1)
+        {
+            Debug.Log("Interacting with NPC: " + currentNPC);
+            // Here you would trigger the dialogue or interaction logic for the NPC
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        // Set currentNPC to the NPC number based on the tag of the collided object
+        if (other.gameObject.CompareTag("NPC1"))
+        {
+            currentNPC = 1;
+        }
+        else if (other.gameObject.CompareTag("NPC2"))
+        {
+            currentNPC = 2;
+        }
+        else if (other.gameObject.CompareTag("NPC3"))
+        {
+            currentNPC = 3;
+        }
+        else if (other.gameObject.CompareTag("NPC4"))
+        {
+            currentNPC = 4;
+        }
+        else if (other.gameObject.CompareTag("NPC5"))
+        {
+            currentNPC = 5;
+        }
+        Debug.Log("Collided with: " + currentNPC);
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerExit2D(Collider2D other)
     {
-        Debug.Log("Collided with: " + collision.gameObject.name);
+        currentNPC = -1; // Reset when exiting NPC range
     }
 }
