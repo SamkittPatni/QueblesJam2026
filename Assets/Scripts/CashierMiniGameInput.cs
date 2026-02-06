71f325e6c0618a12bee776ccdd0664006ffc9375
diyaco-shwany <sdiyaco@gmail.com>
2026-02-06 23:06:46 +0000

    Cashier Mini Game near completion
    All aspects of the minigame are done except for adding the cipher and for allowing a way to exit the game when all 3 calculations are done. No errors.

--- Assets/Scripts/CashierMiniGameInput.cs
+++ Assets/Scripts/CashierMiniGameInput.cs
@@ -7,41 +7,33 @@ using TMPro;
 public class CashierMiniGameInput : MonoBehaviour
 {
     public TMP_InputField inputUser;
-    //private string cashierInput = "";
-    //private float finalTotal = 0f;
-    //private List<KeyCode> numberKeys = new List<KeyCode>{}; 
+
     private List<float> NumbersList = new List<float>{0f,1f,2f,3f,4f,5f,6f,7f,8f,9f};
     private bool condition = false;
+
+    public GameObject customerItem1;
+    public GameObject costTag1;
+    public GameObject customerItem2;
+    public GameObject costTag2;
+    public GameObject customerItem3;
+    public GameObject costTag3;
+
+    private GameObject currentItem;
+    private GameObject currentTag;
+
+
+    public TMP_Text paymentText;
+    private int num = 0;
+    private int score = 0;
+
+    private List<float> ItemCost = new List<float>{239f, 450f, 187f};
+    private List<float> CustomerMoney = new List<float>{381f, 492f, 957f};
     void Start()
     {
-        inputUser.Select();
+        inputUser.interactable=false;
+        StartCoroutine(GameSetUp(customerItem1, costTag1, ItemCost[num].ToString()));
     }
-    /*
-    void Update()
-    {
-        for (int i = 0; i < NumbersList.Count; i++)
-            {
-                if (Input.GetKeyDown(numberKeys[i]))
-                    {
-                        cashierInput += i.ToString();
-                        break;
-                    }
-            }
-        if (Input.GetKeyDown(KeyCode.Backspace))
-        {
-            if (cashierInput.Length > 0)
-            {
-                cashierInput = cashierInput.Remove(cashierInput.Length-1);
-            }
-        }
 
-        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
-        {
-            float.TryParse(cashierInput, out finalTotal);
-            cashierInput = "";
-        }
-    }
-    */
     public void CheckInput(string Input)
     {
         if (Input.Length > 0)
@@ -50,17 +42,113 @@ public class CashierMiniGameInput : MonoBehaviour
             {
                 if (NumbersList[i].ToString() == Input[Input.Length-1].ToString())
                 {
-                    Debug.Log("If Passed 1");
                     condition = true;
                 }
             }
+            if (Input.Length == 1 & Input[0].ToString() == "0")
+            {
+                condition = false;
+            }
             if (condition==false)
             {
-                Debug.Log("If Passed 2");
                 inputUser.text = Input.Remove(Input.Length-1);
             }
             condition = false;
         }
-        Debug.Log(inputUser.text);
+    }
+
+    public void CheckEnter(string Input)
+    {
+        Debug.Log(num);
+        if (CustomerMoney[num-1]-ItemCost[num-1]==float.Parse(Input))
+        {
+            StartCoroutine(WaitLowk(2f, "yay!"));
+            score += 7;
+        }
+        else
+        {
+            StartCoroutine(WaitLowk(2f, "error"));
+            score += -2;
+        }
+
+        inputUser.interactable = false;
+
+        CashierMiniGameObject itemScript = currentItem.GetComponent<CashierMiniGameObject>();
+
+        StartCoroutine(itemScript.Movement(itemScript.OnScreenPosition, itemScript.ExitPosition));
+        CashierMiniGameObject tagScript = currentTag.GetComponent<CashierMiniGameObject>();
+        StartCoroutine(tagScript.Movement(tagScript.OnScreenPosition, tagScript.ExitPosition));
+        inputUser.interactable=false;
+
+        StartCoroutine(Transition());
+        /*
+        if (num == 1)
+        {
+            // Check User Answer
+            DestroyImmediate(customerItem1, true);
+            DestroyImmediate(costTag1, true);
+            if (true)
+            {
+                score++;
+            }
+            StartCoroutine(GameSetUp(customerItem2, costTag2, "68"));
+        }
+        else if (num == 2)
+        {
+            // Check User Answer
+            DestroyImmediate(customerItem2, true);
+            DestroyImmediate(costTag2, true);
+            if (true)
+            {
+                score++;
+            }
+            StartCoroutine(GameSetUp(customerItem3, costTag3, "69"));
+        }
+        else if (num == 3)
+        {
+            // Check User Answer
+            if (true)
+            {
+                score++;
+            }
+            Debug.Log("Game Over");
+        }
+        */
+    }
+
+    private IEnumerator GameSetUp(GameObject itemLocal, GameObject costTagLocal, string value)
+    {
+        currentItem = Instantiate(itemLocal);
+        yield return new WaitForSeconds(1f);
+        currentTag = Instantiate(costTagLocal);
+        yield return new WaitForSeconds(1f);
+        paymentText.SetText(value);
+        inputUser.interactable = true;
+        inputUser.Select();
+        num++;
+    }
+
+    private IEnumerator Transition()
+    {
+        yield return new WaitForSeconds(2f);
+        if (num==1)
+        {
+            StartCoroutine(GameSetUp(customerItem2, costTag2, ItemCost[num].ToString()));
+        }
+        else if (num==2)
+        {
+            StartCoroutine(GameSetUp(customerItem3, costTag3, ItemCost[num].ToString()));
+        }
+        else if (num==3)
+        {
+            Debug.Log("Game Over");
+        }
+    }
+
+    private IEnumerator WaitLowk(float value, string message)
+    {
+        paymentText.SetText(message);
+        inputUser.text = "bingus";
+        yield return new WaitForSeconds(value);
     }
 }
--- Assets/Scripts/CashierMiniGameObject.cs
+++ Assets/Scripts/CashierMiniGameObject.cs
@@ -3,29 +3,35 @@ using System.Collections;
 
 public class CashierMiniGameObject : MonoBehaviour
 {
-    public float speed = 2;
-    public float Duration = 5f;
+    public float speed = 4f;
+    public float Duration = 0.5f;
     [Header("Cutscene Settings")]
     public Vector3 StartPosition;
-    public Vector3 EndPosition;
+    public Vector3 OnScreenPosition;
+    public Vector3 ExitPosition;
 
     void Start()
     {
         transform.position = StartPosition;
-        StartCoroutine(SetUpScene());
+        StartCoroutine(Movement(StartPosition, OnScreenPosition));
     }
-    IEnumerator SetUpScene()
+    public IEnumerator Movement(Vector3 positionA, Vector3 positionB)
     {
         float timePassed = 0f;
         float t = 0f;
         while (timePassed < Duration)
         {
-            transform.position = Vector3.Lerp(StartPosition, EndPosition, t);
+            transform.position = Vector3.Lerp(positionA, positionB, t);
             timePassed += Time.deltaTime;
             t = timePassed/Duration;
             yield return null;
         }
 
-        transform.position = EndPosition;
+        transform.position = positionB;
+    }
+ 
+    public void DestroyObject()
+    {
+        Destroy(gameObject);
     }
 }
