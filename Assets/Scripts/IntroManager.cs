013a91bc1dc10d7e0992837d2558c7fb5af149bb
Samkitt Patni <samkitt.patni@gmail.com>
2026-02-07 00:52:38 +0000

    Implement Intro CutScene

---
+++ Assets/Scripts/IntroManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+using TMPro;
+public class IntroManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject dialogueBox; // Reference to the dialogue box UI element
+    public TMP_Text dialogueText; // Reference to the TextMeshPro text element for displaying dialogue
+    public GameObject scene2;
+    public GameObject spaceShip;
+    public GameObject scene3;
+
+    public GameObject turnPoint;
+
+    void Start()
+    {
+        StartCoroutine(PlayIntro());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator PlayIntro()
+    {
+        yield return new WaitForSeconds(2f); // Wait for 1 second before starting the intro
+        dialogueBox.SetActive(true);
+        yield return new WaitForSeconds(0.5f); // Wait for the panel to appear
+        dialogueText.text = "Dialogue 1";
+        yield return new WaitForSeconds(3f);
+        dialogueText.text = "";
+        dialogueBox.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        scene2.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        turnPoint.SetActive(true);
+        spaceShip.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        dialogueBox.SetActive(true);
+        yield return new WaitForSeconds(0.5f); // Wait for the panel to appear
+        dialogueText.text = "Dialogue 2";
+        yield return new WaitForSeconds(3f);
+        dialogueText.text = "";
+        dialogueBox.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        scene3.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        dialogueBox.SetActive(true);
+        yield return new WaitForSeconds(0.5f); // Wait for the panel to appear
+        dialogueText.text = "Dialogue 3";
+        yield return new WaitForSeconds(3f);
+        dialogueText.text = "";
+        dialogueBox.SetActive(false);
+
+        
+    }
+}
