ffad655aef8b5a4a420b2f92ef9a5deb13e1f63d
Samkitt Patni <samkitt.patni@gmail.com>
2026-02-08 18:47:22 +0000

    Implement Tuna Can in store scene (singular)

---
+++ Assets/Scripts/TunaCanInteract.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class TunaCanInteract : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameManager.Instance.AddTrust(1f);
+            Destroy(gameObject); // Destroy the tuna can after interaction
+            // Here you would trigger the dialogue or interaction logic for the tuna can
+        }
+    }
+}
